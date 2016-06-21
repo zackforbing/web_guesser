ab42fedee6673980dc7c5fa81ec6bb54a4558466
Zack Forbing <zackforbing@gmail.com>
2016-06-21 12:44:56 -0600

    I4: added background color extension

--- web_guesser.rb
+++ web_guesser.rb
@@ -6,21 +6,39 @@ set :number, rand(100)
 get '/' do
   guess = params['guess']
   message = check_guess(guess, settings.number)
-  erb :index, :locals => {:number => settings.number, :message => message, :guess => guess}
-end
-
-def check_guess(guess, number)
-  if guess == nil
-    "Guess the SECRET NUMBER!"
-  elsif guess.to_i > number + 5
-    "WAY Too High!"
-  elsif guess.to_i > number
-    "Too High!"
-  elsif guess.to_i < number - 5
-    "WAY Too Low!"
-  elsif guess.to_i < number
-    "Too Low!"
-  else
-    "You guessed it!<br><br>the SECRET NUMBER is #{number}!"
+  color = bg_color_indicator(guess, settings.number)
+  erb :index, :locals => {:number => settings.number, :message => message,
+    :color => color}
   end
-end
+
+  def check_guess(guess, number)
+    if guess == nil
+      "Guess the SECRET NUMBER!"
+    elsif guess.to_i > number + 5
+      "WAY Too High!"
+    elsif guess.to_i > number
+      "Too High!"
+    elsif guess.to_i < number - 5
+      "WAY Too Low!"
+    elsif guess.to_i < number
+      "Too Low!"
+    else
+      "You guessed it!<br><br>the SECRET NUMBER is #{number}!"
+    end
+  end
+
+  def bg_color_indicator(guess, number)
+    if guess == nil
+      "background: white"
+    elsif guess.to_i > number + 5
+      "background: red"
+    elsif guess.to_i > number
+      "background: coral"
+    elsif guess.to_i < number - 5
+      "background: red"
+    elsif guess.to_i < number
+      "background: coral"
+    else
+      "background: green"
+    end
+  end
